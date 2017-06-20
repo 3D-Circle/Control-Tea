392ddf6268db217aaf4e2529def07caddb8a96f7
Trak Johnson <trakjohnsonweb@gmail.com>
2017-06-20 22:52:42 +0200

    added basic lol api wrapper

---
+++ server/lol_api.py
@@ -0,0 +1,41 @@
+import requests
+
+
+with open('lol_api_key') as f:
+    API_KEY = f.read()
+
+HEADERS = {
+    "Origin": "https://developer.riotgames.com",
+    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+    "X-Riot-Token": f"{API_KEY}",
+    "Accept-Language": "fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4,es;q=0.2"
+}
+
+
+class Summoner:
+    def __init__(self, api_key, username=None):
+        self.root_link = 'https://euw1.api.riotgames.com'
+        self.key = api_key
+        self.username = username
+        self.summoner_id = self.get_summoner_id()
+
+    def get_summoner_id(self):
+        if self.username is None:
+            return 0
+
+        summoner_info_url = '/lol/summoner/v3/summoners/by-name/'
+        r = requests.get(f'{self.root_link}{summoner_info_url}{self.username}', headers=HEADERS)
+        return int(r.json()['id'])
+
+    def current_game(self):
+        current_game_url = '/lol/spectator/v3/active-games/by-summoner/'
+        response = requests.get(f'{self.root_link}{current_game_url}{self.summoner_id}', headers=HEADERS).json()
+        if 'status' in response and int(response["status"]["status_code"]) == 404:
+            return False, None
+        else:
+            return True, response
+
+if __name__ == '__main__':
+    jingjie = Summoner(API_KEY, 'MrJingjie')
+    print(jingjie.summoner_id)
+    print(jingjie.current_game())
