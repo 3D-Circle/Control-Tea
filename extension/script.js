64c2e9ee224de984079f4566ed0d566f6dc46169
Trak Johnson <trakjohnsonweb@gmail.com>
2017-07-06 16:54:16 +0200

    improved weather icon display performance by caching the icon name

--- extension/script.js
+++ extension/script.js
@@ -7,13 +7,22 @@ function set_date_time() {
 }
 
 function set_current_icon() {
+    var skycons = new Skycons({"color": "white"});
+    // cached icon
+    chrome.storage.local.get('latest_weather_icon', function (cached_icon) {
+        if (cached_icon) {
+            skycons.add("weather_icon", cached_icon['latest_weather_icon']);
+        }
+    })
+
     navigator.geolocation.getCurrentPosition(function(position) {
         $.get(root + '/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude).done(
             function(data) {
                 var icon_name = data['icon'];
-                var skycons = new Skycons({"color": "white"});
                 console.log(icon_name.toUpperCase())
+                skycons.remove("weather_icon");
                 skycons.add("weather_icon", Skycons[icon_name.toUpperCase().replace('-', '_')]);
+                chrome.storage.local.set({'latest_weather_icon': icon_name});
                 skycons.play();
             }
         )
